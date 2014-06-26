7a5d2edb21d01ba086605a2b4eab0ceddcecb10d
Mitchel Kelonye <kelonyemitchel@gmail.com>
2014-06-26 19:13:31 +0300

    refactor

--- config/index.js
+++ config/index.js
@@ -21,11 +21,12 @@ var config = {
     'out'
   ],
   S3: {
-    policy: 'ewogICdleHBpcmF0aW9uJzogJzIwMjAtMDEtMDFUMDA6MDA6MDBaJywKICAnY29uZGl0aW9ucyc6IFsgCiAgICB7J2J1Y2tldCc6ICdxdWl6bWFrZXInfSwgCiAgICB7J2FjbCc6ICdwdWJsaWMtcmVhZCd9LAogICAgWydzdGFydHMtd2l0aCcsICckQ29udGVudC1UeXBlJywgJyddLAogICAgWydzdGFydHMtd2l0aCcsICckQ29udGVudC1MZW5ndGgnLCAnJ10sCiAgICBbJ3N0YXJ0cy13aXRoJywgJyRrZXknLCAnJ10sCiAgICBbJ2NvbnRlbnQtbGVuZ3RoLXJhbmdlJywgMCwgMTA0ODU3Nl0KICBdCn0=',
-    signature: 'AeLDP58hSL8UOHa6MzWvFhIRS0w=',
-    url: 'http://quizmaker.s3.amazonaws.com',
-    awsKey: process.env.QUIZMAKER_AWS_ACCESS_KEY_ID,
-    acl: 'public-read'
+    policy: 'ewogICdleHBpcmF0aW9uJzogJzIwMjAtMDEtMDFUMDA6MDA6MDBaJywKICAnY29uZGl0aW9ucyc6IFsgCiAgICB7J2J1Y2tldCc6ICdxdWl6LW1ha2VyLTInfSwgCiAgICB7J2FjbCc6ICdwdWJsaWMtcmVhZCd9LAogICAgWydzdGFydHMtd2l0aCcsICckQ29udGVudC1UeXBlJywgJyddLAogICAgWydzdGFydHMtd2l0aCcsICckQ29udGVudC1MZW5ndGgnLCAnJ10sCiAgICBbJ3N0YXJ0cy13aXRoJywgJyRrZXknLCAnJ10sCiAgICBbJ2NvbnRlbnQtbGVuZ3RoLXJhbmdlJywgMCwgMTA0ODU3Nl0KICBdCn0=',
+    signature: 'iFsnSFbIQHYng+ffNxKVaHeDar0=',
+    url: 'http://quiz-maker-2.s3-us-west-2.amazonaws.com/',
+    key: process.env.QUIZMAKER_AWS_ACCESS_KEY_ID,
+    acl: 'public-read',
+    staticUrl: 'https://s3-us-west-2.amazonaws.com/quiz-maker-2/'  
   }
 };
 
