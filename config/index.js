9dfc1c905a98028553452d7e102a51da0b4aaffd
Mitchel Kelonye <kelonyemitchel@gmail.com>
2014-06-26 04:18:40 +0300

    use s3 POST upload

--- config/index.js
+++ config/index.js
@@ -20,7 +20,13 @@ var config = {
     'app',
     'out'
   ],
-  bucket: 'quiz-maker'
+  S3: {
+    policy: 'ewogICdleHBpcmF0aW9uJzogJzIwMjAtMDEtMDFUMDA6MDA6MDBaJywKICAnY29uZGl0aW9ucyc6IFsgCiAgICB7J2J1Y2tldCc6ICdxdWl6bWFrZXInfSwgCiAgICB7J2FjbCc6ICdwdWJsaWMtcmVhZCd9LAogICAgWydzdGFydHMtd2l0aCcsICckQ29udGVudC1UeXBlJywgJyddLAogICAgWydzdGFydHMtd2l0aCcsICckQ29udGVudC1MZW5ndGgnLCAnJ10sCiAgICBbJ3N0YXJ0cy13aXRoJywgJyRrZXknLCAnJ10sCiAgICBbJ2NvbnRlbnQtbGVuZ3RoLXJhbmdlJywgMCwgMTA0ODU3Nl0KICBdCn0=',
+    signature: 'AeLDP58hSL8UOHa6MzWvFhIRS0w=',
+    url: 'http://quizmaker.s3.amazonaws.com',
+    awsKey: process.env.QUIZMAKER_AWS_ACCESS_KEY_ID,
+    acl: 'public-read'
+  }
 };
 
 if (process.env.NODE_ENV == 'production'){
